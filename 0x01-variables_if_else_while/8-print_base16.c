5874ce69c85447bb338d00e7b21454c71a6d4ff0
jeocoder <josephmukono10@gmail.com>
2023-03-17 13:27:15 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,12 +5,16 @@
  *
  * Return: always zero
  */
+
 int main(void)
 {
-	char i;
+	int i;
+	int j;
 
-	for (i = 'z' ; i >= 'a' ; i--)
-		putchar(i);
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
 	putchar('\n');
 	return (0);
 }
