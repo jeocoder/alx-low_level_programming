a1a87062a5b60402dc30d6cbee7002bd38e8e816
jeocoder <josephmukono10@gmail.com>
2023-03-17 13:35:31 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,16 @@
 int main(void)
 {
 	int i;
-	int j;
 
 	for (i = 0 ; i < 10 ; i++)
+	{
 		putchar(i + '0');
-	for (j = 'a' ; j <= 'f' ; j++)
-		putchar(j);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 }
