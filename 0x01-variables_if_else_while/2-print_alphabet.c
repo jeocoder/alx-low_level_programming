64811745327a9d915b88d7b3e47e6f273c87d409
jeocoder <josephmukono10@gmail.com>
2023-03-17 11:39:28 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 upon success
+ *
+ */
+
+int main(void)
+
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
