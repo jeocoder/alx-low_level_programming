3e46c082983a2a864b452c2ea92181a892bf9bea
jeocoder <josephmukono10@gmail.com>
2023-03-22 06:36:39 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,15 +13,11 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 
 	else if (n == 0)
 	{
-		_putchar('0');
-		_putchar(',');
 		_putchar('0');
 		return (0);
 	}
@@ -29,8 +25,6 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
 
