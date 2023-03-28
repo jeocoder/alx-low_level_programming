797e764e7f14c50b21afe3868b3c34f34a3beb06
jeocoder <josephmukono10@gmail.com>
2023-03-28 03:39:28 -0700

    Task 9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int l = 0;
+	int x = 0;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-	return (dest);
+		while (*(src + l) != '\0')
+		{
+			l++;
+		}
+		for (; x < l ; x++)
+		{
+			dest[x] = src[x];
+		}
+		dest[l] = '\0';
+		return (dest);
 }
