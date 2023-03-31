572b87daf073528d6e355214bbb86c04d978587e
jeocoder <josephmukono10@gmail.com>
2023-03-31 06:24:58 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+char *cap_string(char *)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'Z'))
+			index++;
+		if (str[index - 1] ==  ' '
+	}
+}
