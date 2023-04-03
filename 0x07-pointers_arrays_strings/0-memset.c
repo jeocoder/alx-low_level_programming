2e001742ca326e07571dea9af6f2b50bb1210969
jeocoder <josephmukono10@gmail.com>
2023-04-03 01:49:51 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with a constant byt
+ * @s: memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ * Return: changed array with new value for n bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned char *p = ptr;
+
+	while (num--)
+	{
+		*p++ = (unsigned char)value;
+	}
+	return (ptr);
+}
